c0be9ccfc600e4d91e83976ba5b59e53a9630191
Vladimir <git_109@list.ru>
2021-06-10 20:00:04 +0300

    make the SearchBar component static boilerplate and export it

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,34 @@
-import React from 'reaact'
+import React from 'react'
 import './SearchBar.css'
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review'
+}
+
+export class SearchBar extends React.Component {
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      const sortByOptionValue = sortByOptions[sortByOption]
+      return <li key={sortByOptionValue}>{sortByOption}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input type="text" placeholder="Search Businesses" />
+          <input type="text" placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    )
+  }
+}
