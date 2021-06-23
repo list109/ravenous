14cdf646f769ee3768deb6a83c0587295e63213a
Vladimir <git_109@list.ru>
2021-06-23 18:55:40 +0300

    create js file and set the unique keys to interact with the Yelp api

---
+++ src/util/Yealp.js
@@ -0,0 +1,5 @@
+const clientId = 'h2ayhV07GIf6tEX31j29cw'
+const apiKey =
+  'IoNiWEe1LFoHMhPW3C23ogHi6ih5_rAJqUfjm6Px19rti1nBX8FD-plzKL9jxpQBMnQBZaSwH_z-hN9Ru1rqNV1WoaR0vC9p5ywlR7_hRahgEnBgCIXlNUTgcUzTYHYx'
+
+const Yelp = {}
