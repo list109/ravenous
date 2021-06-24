95f8e549d64aab9601604713dc9bff307e837653
Vladimir <git_109@list.ru>
2021-06-24 18:40:44 +0300

    import yelp tool

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { BusinessList } from '../BusinessList/BusinessList'
 import { SearchBar } from '../SearchBar/SearchBar'
+import { Yelp } from '../../util/Yealp'
 
 const business = {
   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
