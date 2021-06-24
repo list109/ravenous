d7924067e833b22a8d61c3b55f90486088131192
Vladimir <git_109@list.ru>
2021-06-24 18:04:30 +0300

    implement the yelp functionality to fetch data

--- src/util/Yealp.js
+++ src/util/Yealp.js
@@ -1,5 +1,69 @@
-const clientId = 'h2ayhV07GIf6tEX31j29cw'
+// const clientId = 'h2ayhV07GIf6tEX31j29cw'
 const apiKey =
   'IoNiWEe1LFoHMhPW3C23ogHi6ih5_rAJqUfjm6Px19rti1nBX8FD-plzKL9jxpQBMnQBZaSwH_z-hN9Ru1rqNV1WoaR0vC9p5ywlR7_hRahgEnBgCIXlNUTgcUzTYHYx'
 
-const Yelp = {}
+export const Yelp = {
+  async search({ term, location, sortBy }) {
+    const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
+    const endpoint = 'https://api.yelp.com/v3/businesses/search'
+    const urlToFetch = `${corsAnywhere}${endpoint}?term=${term}&location=${location}&sort_by=${sortBy}`
+
+    let response
+
+    try {
+      response = await fetch(urlToFetch, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      })
+    } catch (error) {
+      throw new Error('Network error has occured')
+    }
+
+    if (!response.ok) {
+      throw new Error(`${response.status}:${response.statusText}`)
+    }
+
+    let jsonResponse
+
+    try {
+      jsonResponse = await response.json()
+    } catch (error) {
+      throw new Error('Transformation to json format was failed')
+    }
+    const { businesses = [] } = jsonResponse
+
+    return businesses.map(
+      ({
+        id,
+        image_url: imageSrc = '',
+        name,
+        location,
+        categories,
+        rating,
+        review_count: reviewCount
+      }) => {
+        const { address1: address, city, state, zip_code: zipCode } = location
+        const category = categories.map(({ title }) => title).join(', ')
+
+        return {
+          id,
+          imageSrc,
+          name,
+          address,
+          city,
+          state,
+          zipCode,
+          category,
+          rating,
+          reviewCount
+        }
+      }
+    )
+  }
+}
+
+// handle uncaught failed fetch through
+window.addEventListener('unhandledrejection', event => {
+  console.log(event.reason.message)
+})
