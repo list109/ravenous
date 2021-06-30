be4249b9a64b832472bcff1b07c2d6bb11ff611f
Vladimir <git_109@list.ru>
2021-06-30 20:01:31 +0300

    handle a fetch error to notify users depending on an error status

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,39 @@ import { SearchBar } from '../SearchBar/SearchBar'
 import { Yelp } from '../../util/Yealp'
 
 export class App extends React.Component {
-  state = { businesses: [] }
+  state = { businesses: [], errorMessage: '' }
 
   searchYelp = ({ term, location, sortBy }) => {
-    Yelp.search({ term, location, sortBy }).then(businesses => {
-      this.setState({ businesses })
-    })
+    Yelp.search({ term, location, sortBy })
+      .then(businesses => {
+        this.setState({ businesses, errorMessage: '' })
+      })
+      .catch(error => {
+        this.setState({ errorMessage: this.getErrorMessage(error) })
+      })
+  }
+
+  getErrorMessage({ response, message }) {
+    switch (response.status) {
+      case 400:
+        return 'Please, try to change some of the fields value'
+      default:
+        return message
+    }
   }
 
   render() {
+    const { businesses, errorMessage } = this.state
+
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} />
+        {errorMessage ? (
+          <p className="error-message">{errorMessage}</p>
+        ) : (
+          <BusinessList businesses={businesses} />
+        )}
       </div>
     )
   }
