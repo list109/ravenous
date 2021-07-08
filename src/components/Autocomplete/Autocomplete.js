d7030a4211dba60fa1cc33ef96da35aa528c4beb
Vladimir <git_109@list.ru>
2021-07-08 18:12:06 +0300

    make some template for the autocomplete to render

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,3 +1,16 @@
 import React from 'react'
 
-function Autocomplete() {}
+function Autocomplete({ children, options, onClick }) {
+  return (
+    <div className="Autocomplete">
+      {children}
+      <ul className="Autocomplete-list">
+        {options.map(option => (
+          <li className="Autocomplete-option" onClick={() => onClick(option)}>
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
