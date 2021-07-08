cee0b0aafaeeb88cdce368346e44aa657286275a
Vladimir <git_109@list.ru>
2021-07-08 20:45:45 +0300

    syntax enhancement for the autocomplete component

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Autocomplete.css'
 
-function Autocomplete({ children, options, onClick }) {
+export function Autocomplete({ children, options = [], onClick }) {
+  const autocompleteList = Boolean(options.length) && (
+    <ul className="Autocomplete-list">
+      {options.map(option => (
+        <li className="Autocomplete-option" onClick={() => onClick(option)}>
+          {option}
+        </li>
+      ))}
+    </ul>
+  )
+
   return (
     <div className="Autocomplete">
       {children}
-      <ul className="Autocomplete-list">
-        {options.map(option => (
-          <li className="Autocomplete-option" onClick={() => onClick(option)}>
-            {option}
-          </li>
-        ))}
-      </ul>
+      {autocompleteList}
     </div>
   )
 }
