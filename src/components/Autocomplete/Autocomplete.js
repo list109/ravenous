b6e42562561b9bd422b9fe4a0db07e5d262c3e17
Vladimir <git_109@list.ru>
2021-07-10 15:10:08 +0300

    make event the listeners as detached methods to split logic of clicks and presses

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -2,10 +2,19 @@ import React from 'react'
 import './Autocomplete.css'
 
 export function Autocomplete({ children, options = [], isOpen, onOption, onBlur, onFocus }) {
+  const handleClick = option => onOption(option)
+  const handleKeyPress = ({ option, code }) => code === 'Enter' && onOption(option)
+
   const autocompleteList = isOpen && options.length > 0 && (
     <ul className="Autocomplete-list">
       {options.map((option, i) => (
-        <li key={i} className="Autocomplete-option" onClick={() => onClick(option)} tabIndex="0">
+        <li
+          key={i}
+          className="Autocomplete-option"
+          onClick={() => handleClick(option)}
+          onKeyPress={({ code }) => handleKeyPress({ option, code })}
+          tabIndex="0"
+        >
           {option}
         </li>
       ))}
