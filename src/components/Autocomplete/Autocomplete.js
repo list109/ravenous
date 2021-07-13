75dfbbb8d477f22dba71d6787ad507b08a56c410
Vladimir <git_109@list.ru>
2021-07-13 19:30:26 +0300

    forward a ref into the Autocomplete of location component to access the dom autocomplete list in order to focus on the list's first element from the location field

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,40 +1,34 @@
 import React from 'react'
 import './Autocomplete.css'
 
-export function Autocomplete({
-  children,
-  options = [],
-  isOpen,
-  onBlur,
-  onFocus,
-  onClick,
-  onKeyDown
-}) {
-  const handleClick = option => onClick(option)
-  const handleKeyDown = ({ option, code, elem }) => {
-    onKeyDown({ option, code, elem })
+export const Autocomplete = React.forwardRef(
+  ({ children, options = [], isOpen, onBlur, onFocus, onClick, onKeyDown }, ref) => {
+    const handleClick = option => onClick(option)
+    const handleKeyDown = ({ option, code, elem }) => {
+      onKeyDown({ option, code, elem })
+    }
+
+    const autocompleteList = isOpen && options.length > 0 && (
+      <ul className="Autocomplete-list" ref={ref}>
+        {options.map((option, i) => (
+          <li
+            key={i}
+            className="Autocomplete-option"
+            onClick={() => handleClick(option)}
+            onKeyDown={({ code, target: elem }) => handleKeyDown({ option, code, elem })}
+            tabIndex="0"
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    )
+
+    return (
+      <div className="Autocomplete" onBlur={onBlur} onFocus={onFocus}>
+        {children}
+        {autocompleteList}
+      </div>
+    )
   }
-
-  const autocompleteList = isOpen && options.length > 0 && (
-    <ul className="Autocomplete-list">
-      {options.map((option, i) => (
-        <li
-          key={i}
-          className="Autocomplete-option"
-          onClick={() => handleClick(option)}
-          onKeyDown={({ code, target: elem }) => handleKeyDown({ option, code, elem })}
-          tabIndex="0"
-        >
-          {option}
-        </li>
-      ))}
-    </ul>
-  )
-
-  return (
-    <div className="Autocomplete" onBlur={onBlur} onFocus={onFocus}>
-      {children}
-      {autocompleteList}
-    </div>
-  )
-}
+)
