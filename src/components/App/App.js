205d76e5ccac3a47fb84910cc6bd9aa5520a255b
Vladimir <git_109@list.ru>
2021-08-05 23:49:29 +0300

    move all the the Search bar's state logic to the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,190 @@ import { Yelp } from '../../util/Yealp'
 import { Loading } from '../Loading/Loading'
 
 export class App extends React.Component {
-  state = { businesses: [], errorMessage: '', isRequestRun: false }
+  state = {
+    businesses: [],
+    errorMessage: 'Start with a location value. The rest is optional.',
+    isRequestRun: false,
 
-  setErrorMessage = message => {
+    sortBy: 'best_match',
+    term: '',
+    radius: '',
+    onlyOpened: false,
+
+    location: '',
+    isLocationInvalid: false,
+    locationOptions: [],
+    isLocationOptionsOpen: false,
+    locationFocusedOptionIndex: 0
+  }
+
+  locationRef = React.createRef()
+  locationSearchTimeOutId = null
+
+  handleSortByChange = sortOption => {
+    const isValid = this.checkFormValidity()
+    this.setState({ sortBy: sortOption }, isValid ? this.handleSubmit : undefined)
+  }
+
+  handleTermChange = value => {
     this.setState({
-      errorMessage: message
+      term: value
     })
   }
 
+  handleRadiusChange = value => {
+    if (value > 40000) return
+
+    this.setState({
+      radius: value
+    })
+  }
+
+  handleStatusChange = value => {
+    this.setState({
+      onlyOpened: value
+    })
+  }
+
+  handleLocationChange = value => {
+    this.setState({
+      location: value,
+      isLocationInvalid: false
+    })
+
+    clearTimeout(this.locationSearchTimeOutId)
+
+    if (value === '') {
+      this.setState({
+        locationOptions: [],
+        locationFocusedOptionIndex: 0
+      })
+      return
+    }
+
+    this.locationSearchTimeOutId = setTimeout(
+      () => this.searchLocationOptions({ location: value, limit: 5 }),
+      700
+    )
+  }
+
+  searchLocationOptions = ({ location, limit }) => {
+    Yelp.searchBusinesses({ location, limit })
+      .then(businesses => {
+        const options = businesses.map(({ address, city, state, country }) => {
+          return `${address}${city && `, ${city}`}${state && `, ${state}`} ${
+            country && `, ${country}`
+          }`
+        })
+
+        if (this.state.location === location && this.state.isRequestRun === false) {
+          this.setState({
+            locationOptions: [this.state.location, ...options],
+            isLocationOptionsOpen: document.activeElement === this.locationRef.current
+          })
+        }
+      })
+      .catch(error => {
+        this.setState({
+          locationOptions: []
+        })
+      })
+  }
+
+  handleLocationFocus = () => {
+    this.setState({
+      isLocationOptionsOpen: true
+    })
+  }
+
+  handleLocationUnfocus = () => {
+    this.setState({
+      isLocationOptionsOpen: false
+    })
+  }
+
+  handleLocationOptionClick = option => {
+    this.setLocationOption(option)
+  }
+
+  setLocationOption = option => {
+    this.setState({
+      location: option,
+      locationOptions: [],
+      locationFocusedOptionIndex: 0
+    })
+    this.locationRef.current.focus()
+  }
+
+  handleLocationKey = e => {
+    const { locationOptions, locationFocusedOptionIndex: index } = this.state
+    const getNextIndex = index => (index + 1 === locationOptions.length ? 0 : index + 1)
+    const getPrevIndex = index => (index - 1 < 0 ? locationOptions.length - 1 : index - 1)
+
+    switch (e.code) {
+      case 'Enter':
+        if (locationOptions.length === 0) return
+        this.setLocationOption(locationOptions[index])
+        e.preventDefault()
+        break
+      case 'ArrowUp':
+        this.setState({
+          locationFocusedOptionIndex: getPrevIndex(index)
+        })
+        e.preventDefault()
+        break
+      case 'ArrowDown':
+        this.setState({
+          locationFocusedOptionIndex: getNextIndex(index)
+        })
+        e.preventDefault()
+        break
+      case 'Tab':
+        if (locationOptions.length === 0) return
+        this.setState({
+          locationFocusedOptionIndex: e.shiftKey ? getPrevIndex(index) : getNextIndex(index)
+        })
+        e.preventDefault()
+        break
+      case 'Escape':
+        this.setState({
+          isLocationOptionsOpen: false
+        })
+        break
+      default:
+        return ''
+    }
+  }
+
+  handleLocationOptionOver = index => {
+    this.setState({
+      locationFocusedOptionIndex: index
+    })
+  }
+
+  checkFormValidity() {
+    return this.locationRef.current.checkValidity()
+  }
+  // 1. client-side validation
+  handleInvalid = e => {
+    e.preventDefault()
+
+    this.setState({
+      isLocationInvalid: true,
+      errorMessage: 'Please, fill the location field in'
+    })
+  }
+  // 2. submiting if no constrains validation have been found unless the invalid value is already was checked and set before
+  handleSubmit = e => {
+    e?.preventDefault()
+    if (this.state.isLocationInvalid) return
+
+    this.setState({ locationOptions: [] })
+
+    const { sortBy, term, location, radius, onlyOpened } = this.state
+    this.searchYelp({ sortBy, term, location, radius, onlyOpened })
+  }
+  // 3. post-server-side validation is aplyed on a response
   searchYelp = ({ term, location, sortBy, radius, onlyOpened }) => {
     if (this.state.isRequestRun) return
     this.setState({
@@ -25,8 +201,12 @@ export class App extends React.Component {
       .then(businesses => {
         this.setState({ businesses, errorMessage: '' })
       })
-      .catch(error => {
-        this.setState({ errorMessage: this.getErrorMessage(error) })
+      // server-side validation
+      .catch(({ status, message }) => {
+        this.setState({
+          errorMessage: this.getErrorMessage({ status, message }),
+          isLocationInvalid: status === 'LOCATION_NOT_FOUND'
+        })
       })
       .finally(() => this.setState({ isRequestRun: false }))
   }
@@ -41,7 +221,20 @@ export class App extends React.Component {
   }
 
   render() {
-    const { businesses, errorMessage, isRequestRun } = this.state
+    const {
+      businesses,
+      errorMessage,
+      isRequestRun,
+      sortBy,
+      term,
+      radius,
+      onlyOpend,
+      location,
+      locationOptions,
+      isLocationOptionsOpen,
+      locationFocusedOptionIndex
+    } = this.state
+
     const output = errorMessage ? (
       <p className="error-message">{errorMessage}</p>
     ) : (
@@ -51,7 +244,30 @@ export class App extends React.Component {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar searchYelp={this.searchYelp} setErrorMessage={this.setErrorMessage} />
+        <SearchBar
+          sortBy={sortBy}
+          onSortByChange={this.handleSortByChange}
+          term={term}
+          onTermChange={this.handleTermChange}
+          radius={radius}
+          onRadiusChange={this.handleRadiusChange}
+          onlyOpend={onlyOpend}
+          onStatusChange={this.handleStatusChange}
+          location={location}
+          locationRef={this.locationRef}
+          locationOptions={locationOptions}
+          isLocationOptionsOpen={isLocationOptionsOpen}
+          locationFocusedOptionIndex={locationFocusedOptionIndex}
+          onLocationChange={this.handleLocationChange}
+          onLocationFocus={this.handleLocationFocus}
+          onLocationUnfocus={this.handleLocationUnfocus}
+          onLocationOptionClick={this.handleLocationOptionClick}
+          onLocationOptionOver={this.handleLocationOptionOver}
+          onLocationKey={this.handleLocationKey}
+          isLocationInvalid={this.state.isLocationInvalid}
+          onInvalid={this.handleInvalid}
+          onSubmit={this.handleSubmit}
+        />
         {isRequestRun ? Loading : output}
       </div>
     )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import './SearchBar.css'
 import { Autocomplete } from '../Autocomplete/Autocomplete'
-import { Yelp } from '../../util/Yealp'
 
 export class SearchBar extends React.Component {
-  state = {
-    sortBy: 'best_match',
-    term: '',
-    location: '',
-    radius: '',
-    onlyOpened: false,
-    locationOptions: [],
-    locationOptionsIsOpen: false,
-    locationFocusedOptionIndex: 0
-  }
-
-  locationUnfocusTimeOutId = null
-  locationSearchTimeOutId = null
-  locationRef = React.createRef()
-
-  isWaitingMainSearch = false
-
   sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
@@ -28,162 +10,18 @@ export class SearchBar extends React.Component {
     Nearest: 'distance'
   }
 
-  handleSortByChange = sortOption => {
-    this.setState({ sortBy: sortOption })
+  handleSortByChange = sortOption => this.props.onSortByChange(sortOption)
 
-    const { term, location, radius, onlyOpened } = this.state
-    this.props.searchYelp({ sortBy: sortOption, term, location, radius, onlyOpened })
-  }
+  handleTermChange = ({ target }) => this.props.onTermChange(target.value)
 
-  handleTermChange = ({ target }) => {
-    this.setState({
-      term: target.value
-    })
-  }
+  handleRadiusChange = ({ target }) => this.props.onRadiusChange(target.value)
 
-  handleLocationChange = ({ target }) => {
-    this.setState({
-      location: target.value
-    })
+  handleStatusChange = ({ target }) => this.props.onStatusChange(target.checked)
 
-    this.isWaitingMainSearch = false
-    clearTimeout(this.locationSearchTimeOutId)
-
-    if (target.value === '') {
-      this.setState({
-        locationOptions: [],
-        locationFocusedOptionIndex: 0
-      })
-      return
-    }
-
-    this.locationSearchTimeOutId = setTimeout(
-      () => this.searchLocationOptions({ location: target.value, limit: 5 }),
-      700
-    )
-  }
-
-  searchLocationOptions = ({ location, limit }) => {
-    Yelp.searchBusinesses({ location, limit })
-      .then(businesses => {
-        const options = businesses.map(({ address, city, state, country }) => {
-          return `${address}${city && `, ${city}`}${state && `, ${state}`} ${
-            country && `, ${country}`
-          }`
-        })
-
-        if (this.state.location === location && this.isWaitingMainSearch === false) {
-          this.setState({ locationOptions: [this.state.location, ...options] })
-        }
-      })
-      .catch(error => {
-        this.setState({
-          locationOptions: []
-        })
-      })
-  }
-
-  handleLocationClickOption = option => {
-    this.setLocationOption(option)
-  }
-
-  setLocationOption = option => {
-    this.setState({
-      location: option,
-      locationOptions: [],
-      locationFocusedOptionIndex: 0
-    })
-    this.locationRef.current.focus()
-  }
-
-  handleLocationUnfocuse = () => {
-    this.locationUnfocusTimeOutId = setTimeout(() => {
-      this.setState({
-        locationOptionsIsOpen: false
-      })
-    })
-  }
-
-  handleLocationFocuse = () => {
-    clearTimeout(this.locationUnfocusTimeOutId)
-
-    this.setState({
-      locationOptionsIsOpen: true
-    })
-  }
-
-  handleLocationKey = e => {
-    const { locationOptions, locationFocusedOptionIndex: index } = this.state
-    const getNextIndex = index => (index + 1 === locationOptions.length ? 0 : index + 1)
-    const getPrevIndex = index => (index - 1 < 0 ? locationOptions.length - 1 : index - 1)
-
-    switch (e.code) {
-      case 'Enter':
-        if (locationOptions.length === 0) return
-        this.setLocationOption(locationOptions[index])
-        e.preventDefault()
-        break
-      case 'ArrowUp':
-        this.setState({
-          locationFocusedOptionIndex: getPrevIndex(index)
-        })
-        e.preventDefault()
-        break
-      case 'ArrowDown':
-        this.setState({
-          locationFocusedOptionIndex: getNextIndex(index)
-        })
-        e.preventDefault()
-        break
-      case 'Tab':
-        if (locationOptions.length === 0) return
-        this.setState({
-          locationFocusedOptionIndex: e.shiftKey ? getPrevIndex(index) : getNextIndex(index)
-        })
-        e.preventDefault()
-        break
-      default:
-        return ''
-    }
-  }
-
-  handleLocationOverOption = index => {
-    this.setState({
-      locationFocusedOptionIndex: index
-    })
-  }
-
-  handleRadiusChange = ({ target }) => {
-    if (target.value > 40000) return
-
-    this.setState({
-      radius: target.value
-    })
-  }
-
-  handleStatusChange = ({ target }) => {
-    this.setState({
-      onlyOpened: target.checked
-    })
-  }
+  handleLocationChange = ({ target }) => this.props.onLocationChange(target.value)
 
   getSortByClass(sortOption) {
-    return sortOption === this.state.sortBy ? 'active' : ''
-  }
-
-  handleSearch = e => {
-    e.preventDefault()
-    this.setState({ locationOptions: [] })
-    this.isWaitingMainSearch = true
-
-    const { sortBy, term, location, radius, onlyOpened } = this.state
-    this.props.searchYelp({ sortBy, term, location, radius, onlyOpened })
-  }
-
-  handleInvalid = e => {
-    e.preventDefault()
-
-    this.props.setErrorMessage('Please, fill the location field in')
+    return sortOption === this.props.sortBy ? 'active' : ''
   }
 
   renderSortByOptions() {
@@ -202,36 +40,53 @@ export class SearchBar extends React.Component {
   }
 
   render() {
+    const {
+      term,
+      radius,
+      location,
+      locationRef,
+      locationOptions,
+      isLocationOptionsOpen,
+      locationFocusedOptionIndex,
+      onLocationUnfocus,
+      onLocationFocus,
+      onLocationKey,
+      onLocationOptionClick,
+      onLocationOptionOver,
+      isLocationInvalid,
+      onInvalid,
+      onSubmit
+    } = this.props
+
     return (
-      <form className="SearchBar" onSubmit={this.handleSearch} onInvalid={this.handleInvalid}>
+      <form className="SearchBar" onSubmit={onSubmit} onInvalid={onInvalid}>
         <div className="SearchBar-sort-options">
           <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
           <input
             type="text"
-            value={this.state.term}
+            value={term}
             onChange={this.handleTermChange}
             placeholder="Search Businesses"
           />
           <Autocomplete
-            options={this.state.locationOptions}
-            onBlur={this.handleLocationUnfocuse}
-            onFocus={this.handleLocationFocuse}
-            onOptionFocus={this.handleLocationOptionFocus}
-            onOptionClick={this.handleLocationClickOption}
-            onOptionPointerOver={this.handleLocationOverOption}
-            isOpen={this.state.locationOptionsIsOpen}
-            focusedOptionIndex={this.state.locationFocusedOptionIndex}
+            options={locationOptions}
+            isOpen={isLocationOptionsOpen}
+            focusedOptionIndex={locationFocusedOptionIndex}
+            onBlur={onLocationUnfocus}
+            onFocus={onLocationFocus}
+            onOptionClick={onLocationOptionClick}
+            onOptionPointerOver={onLocationOptionOver}
           >
-            <p className="SearchBar-location">
+            <p className={`SearchBar-location ${isLocationInvalid ? 'invalid' : undefined}`}>
               <input
                 type="text"
-                value={this.state.location}
+                value={location}
                 onChange={this.handleLocationChange}
-                onKeyDown={this.handleLocationKey}
+                onKeyDown={onLocationKey}
                 placeholder="Where?"
-                ref={this.locationRef}
+                ref={locationRef}
                 required={true}
               />
             </p>
@@ -245,7 +100,7 @@ export class SearchBar extends React.Component {
                 type="number"
                 min="0"
                 max="40000"
-                value={this.state.radius}
+                value={radius}
                 onChange={this.handleRadiusChange}
               />
               Specify a radius of the search area (max 40000 m)
